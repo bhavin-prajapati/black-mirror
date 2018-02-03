1f5019b7b1e6e143ac4f1c8701701971657d2866
Bhavin Prajapati <bhavin.prajapati@gmail.com>
2018-02-02 19:48:34 -0500

    Make request to web from alexa

--- alexa-skill/lambda/black-mirror/handlers/handlers.js
+++ alexa-skill/lambda/black-mirror/handlers/handlers.js
@@ -1,3 +1,4 @@
+const fetch = require('isomorphic-fetch');
 const { keys } = require('../constants/speech.js');
 const { states } = require('../constants/states.js');
 
@@ -18,18 +19,27 @@ const handlers = {
         this.emitWithState('AMAZON.YesIntent');
     },
     'HomeIntent': function () {
+        this.handler.state = states.HOME_MODE;
+        fetch("http://localhost:3000/show/home")
+        .then((response) => {
+            console.log("home", response);
+        });
         this.emit(':ask', this.t(keys.HOME_STATE), this.t(keys.LAUNCH_REQUEST_REPROMPT));
     },
     'NewsIntent': function () {
+        this.handler.state = states.NEWS_MODE;
         this.emit(':ask', this.t(keys.NEWS_STATE), this.t(keys.LAUNCH_REQUEST_REPROMPT));
     },
     'CalendarIntent': function () {
+        this.handler.state = states.CALENDAR_MODE;
         this.emit(':ask', this.t(keys.CALENDAR_STATE), this.t(keys.LAUNCH_REQUEST_REPROMPT));
     },
     'WeatherIntent': function () {
+        this.handler.state = states.WEATHER_MODE;
         this.emit(':ask', this.t(keys.WEATHER_STATE), this.t(keys.LAUNCH_REQUEST_REPROMPT));
     },
     'ClockIntent': function () {
+        this.handler.state = states.CLOCK_MODE;
         this.emit(':ask', this.t(keys.CLOCK_STATE), this.t(keys.LAUNCH_REQUEST_REPROMPT));
     },
     HelpIntent() {
